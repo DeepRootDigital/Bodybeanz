0acac6c116c178a2e53e631026f08e5625ba6a0c
Nicholas Koskowski <Nick@businessonmarketst.com>
2014-02-03 10:06:22 -0800

    Updated Website Files

--- Website/index.php
+++ Website/index.php
@@ -1,172 +1,98 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Body Beanz</title>
-  <link href="<?php echo get_template_directory_uri(); ?>/new-hp.css" rel="stylesheet" type="text/css" />
- <link type="text/css" rel="stylesheet" href="http://fast.fonts.com/cssapi/ae0e63e9-c5cf-4919-a2b1-2fdf7c96485b.css"/>
-  <script type="text/javascript" src="http://fast.fonts.net/jsapi/ae0e63e9-c5cf-4919-a2b1-2fdf7c96485b.js"></script>
-<?php wp_head(); ?>
- <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js"></script>
-<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/fancybox/source/jquery.fancybox.css?v=2.1.3" type="text/css" media="screen" />
-<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/fancybox/source/jquery.fancybox.pack.js?v=2.1.3"></script>
-<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/fancybox/source/helpers/jquery.fancybox-buttons.css?v=1.0.5" type="text/css" media="screen" />
-<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/fancybox/source/helpers/jquery.fancybox-buttons.js?v=1.0.5"></script>
-<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/fancybox/source/helpers/jquery.fancybox-media.js?v=1.0.5"></script>
-<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/fancybox/source/helpers/jquery.fancybox-thumbs.css?v=1.0.7" type="text/css" media="screen" />
-<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/fancybox/source/helpers/jquery.fancybox-thumbs.js?v=1.0.7"></script>
-<script type="text/javascript">
-$(document).ready(function() {
-   $('#slideFrame .slide').hide();
-$('#slideFrame .slide:first').fadeIn('slow');
-setInterval(function() {
-   $('#slideFrame .slide').hide();
-$('#slideFrame .slide:last').fadeIn('slow');
-$('#slideFrame .slide:first').before($('#slideFrame .slide:last'));
-}, 3000);
-
-});
-</script>
-<script type="text/javascript">
-$(document).ready(function() {
-  $('.fancybox-media').fancybox({
-    openEffect  : 'none',
-    closeEffect : 'none',
-    helpers : {
-      media : {}
-    }
-
-  });
-  $("a#inline").fancybox({
-    'hideOnContentClick': true
-  });
-});
-$(document).ready(function() {
-
-  /* This is basic - uses default settings */
-  
-  $("a#single_image").fancybox({
-  'scrolling' : 'yes',
-'width' : '850',
-'autoscale' : 'false',
-'autoDimensions'  : 'false',
-});
-});
-</script>
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-39326360-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
-</head>
-<body>
-  <div class="container">
-    <div class="top-content">
-      <div class="top-container">
-        <div class="header">
-          <a href="#"><img class="logo" src="<?php echo get_template_directory_uri(); ?>/images/logo.png" /></a>
-          <div class="nav">
-            <?php wp_nav_menu( array('menu' => 'Home Menu' )); ?>
-          </div>
-        </div>
-        <div id="slideFrame">
-          <div class="slide">
-<img src="http://bodybeanz.com/wp-content/uploads/2013/08/Slide_1.png">
-        </div>   
-<div class="slide">
-<img src="http://bodybeanz.com/wp-content/uploads/2013/08/BB_Slide2.jpg">
-<div class="overlay">
-<h2>
-Battle <span>Beanz</span>
-<img src="<?php echo get_template_directory_uri(); ?>/images/leafy.png">
-</h2>
-<p>help strengthen your immune system with antioxidants</p>
-<a href="http://bodybeanz.com/learn-more"><img class="lm-mm" src="<?php echo get_template_directory_uri(); ?>/images/learn-more-slider.png"></a>
+<?php include 'new-hp.php'; ?>
+<div class="slider">
+	<div class="container">
+		<div class="slides">
+			<div class="slide-1">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/slide-1.png">
+			</div>
+			<div class="slide-2">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/slide-2.png">
+			</div>
+		</div>
+	</div>
 </div>
-</div>
-<div class="slide">
-<img src="http://bodybeanz.com/wp-content/uploads/2013/08/BB_Slide3.jpg">
-<div class="overlay">
-<h2>
-Belly <br><span>Beanz</span>
-<img src="<?php echo get_template_directory_uri(); ?>/images/leafy.png">
-</h2>
-<p>promote healthy blood sugar for more energy, less fat</p>
-<a href="http://bodybeanz.com/learn-more"><img class="lm-mm" src="<?php echo get_template_directory_uri(); ?>/images/learn-more-slider.png"></a>
-</div>
-</div>
-<div class="slide">
-<img src="http://bodybeanz.com/wp-content/uploads/2013/08/BB_Slide4.jpg">
-<div class="overlay">
-<h2>
-Breakdown <span>Beanz</span>
-<img src="<?php echo get_template_directory_uri(); ?>/images/leafy.png">
-</h2>
-<p>improve your digestion & nutrient absorption with probiotics</p>
-<a href="http://bodybeanz.com/learn-more"><img class="lm-mm" src="<?php echo get_template_directory_uri(); ?>/images/learn-more-slider.png"></a>
-</div>
-</div>
-<div class="slide">
-<img src="http://bodybeanz.com/wp-content/uploads/2013/08/BB_Slide5.jpg">
-<div class="overlay">
-<h2>
-Boomer <span>Beanz</span>
-<img src="<?php echo get_template_directory_uri(); ?>/images/leafy.png">
-</h2>
-<p>support your heart health with omegas</p>
-<a href="http://bodybeanz.com/learn-more"><img class="lm-mm" src="<?php echo get_template_directory_uri(); ?>/images/learn-more-slider.png"></a>
-</div>
-</div>   
-    </div>
-        </div>
-      </div>
-    </div>
-    <div class="bottom-content">
-      <div class="bottom-container">
-        <div class="promo box">
-          <a href="http://bodybeanz.com/learn-more"><img class="beanz-icon2" src="<?php echo get_template_directory_uri(); ?>/images/beanz-icon2.png" alt="Body Beanz" /></a>
-          <a href="http://bodybeanz.com/learn-more"><img class="learn-more-ribbon" src="<?php echo get_template_directory_uri(); ?>/images/learn-more-ribbon.png" alt="" /></a>
-        </div>
-        <div class="the-science box">
-          <div class="video">
-            <a class="fancybox-media" href="http://player.vimeo.com/video/69434445"><img class="video" src="http://bodybeanz.com/wp-content/uploads/2013/08/video.png" alt="" /></a>
-            <img class="science-text-leaf" src="<?php echo get_template_directory_uri(); ?>/images/science-text-leaf.png" alt="" />
-            <p>This is why our Body Beanz superfood nutrients are better than your vitamins! Click now to watch this short video that explains it all!</p>
-            </div>
-          </div>
-          <div class="the-facts box">
-            <img class="beanz-bag" src="<?php echo get_template_directory_uri(); ?>/images/beanz-bag.png" />
-            <div class="fact-list">
-              <div class="left">
-                <ul>
-                  <li>natural</li>
-                  <li><span>supplements</span></li>
-                </ul>
-                <ul>
-                  <li>100%</li>
-                  <li><span>superfoods</span></li>
-                </ul>
-              </div>
-              <div class="right">
-                <ul>
-                  <li>zero</li>
-                  <li><span>chemicals</span></li>
-                </ul>
-                <ul>
-                  <li>easy</li>
-                  <li><span>packs</span></li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <?php get_footer(); ?>
+<div class="content">
+	<div class="container">
+		<a href="http://bodybeanz.com/our-formulas/"><img class="banner" src="<?php echo get_template_directory_uri(); ?>/images/banner.png"></a>
+		<div class="box-wrap">
+			<div class="box">
+				<a href="http://bodybeanz.com/learn-more/"><img src="<?php echo get_template_directory_uri(); ?>/images/promo-back.png"></a>
+			</div>
+			<div class="box">
+				<h2>People <span>Like</span> Us</h2>
+				<img src="<?php echo get_template_directory_uri(); ?>/images/quote-hr.png">
+				<div class="testimonial-area">
+					<div class="testimonial-wrap">
+						<div class="testimonial-quote">
+							<p>"I was looking quite extensively for multiple daily supplements that were of honest quality, vegan, affordable and that I could take in only a few pills a day. I received a gift that came with a Body Beanz coupon and found exactly what I needed. In one cute, little packet every morning, I get my omegas, antioxidants, probiotics, and help in metabolizing fats and sugars, in just six easy-to-swallow, don't-leave-an-awful-taste-in-your-mouth pills. THANK YOU Body Beanz for being perfect, convenient, energizing - and affordable!"
+								<br/><b>Angela Rossi - Billerica, MA.</b>
+							</p>
+						</div>
+						<div class="testimonial-quote">
+							<p>I tried Body Beanz expecting them to be just like all other vitamins. I was pleasantly surprised to find they're NOT. I feel so much better and I believe they have helped in my weight loss goals as well. I highly recommend these to anyone looking for an all natural vitamin. The customer service is fantastic and very personalized which is a nice change of pace.
+								<br/><b>Michelle Rezulak - Forreston, IL.</b>
+							</p>
+						</div>
+						<div class="testimonial-quote">
+							<p>I just wanted to take a moment to thank you for making such a wonderful product. I've only been using BodyBeanz for about a week, but I can already feel a difference. I feel awake throughout the day without the caffeine jitters and crash. I feel great about the product being all natural and the daily pouches are so much more convenient than using several bottles, especially when I travel for work. I'm proud to have family members looking into adding the product to their lives. 
+								Thank you again. 
+								<br/><b>Stephanie Elko - Burleson, TX.</b>
+							</p>
+						</div>
+					</div>
+				</div>
+					<!--
+						Placeholder for future update
+					<div class="quote-nav">
+						<span class="">Read more</span>
+						<span>2</span>
+						<span>3</span>
+						<span>4</span>
+					</div> 
+				-->
+			</div>
+			<div class="box">
+				<a href="http://bodybeanz.com/about-us/"><img class="portrait" src="<?php echo get_template_directory_uri(); ?>/images/scott.png"></a>
+				<p><b>I WAS AT A WELLNESS CONFERENCE IN EARLY 2012...</b></p>
+				<p>Speaker after speaker got up on stage in front of a few thousand attendees in Orange County, CA and wowed me with fascinating facts, figures, and stories...<br/> [<a href="http://bodybeanz.com/about-us/"> read full story </a>]</p>
+			</div>
+		</div>
+		<div class="center-wrap">
+			<div class="video">
+				<iframe src="//player.vimeo.com/video/69434445" width="800" height="595" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+			</div>
+			<div class="longbox">
+				<h3><span>Free</span> Helpful Printable Grocery Guide</h3>
+				<hr/>
+				<div class="button-clear">
+					<img class="book" src="<?php echo get_template_directory_uri(); ?>/images/book.png">
+					<p>"<span>10 chemical food ingredients</span> you must
+						<span class="o">avoid</span> at the grocery store... and why!"</p>
+					</div>
+					<button><a class="tooltip" title="Coming Soon">Yes, Give me the Cheat Sheet</a></button>
+				</div>
+				<div class="longbox">
+					<div class="half">
+						<h4>Popular <span>Stuff to read</span></h4>
+						<hr/>
+						<ul>
+							<li><span>If you only knew what was in your vitamins.</span></li>
+							<li><span>Yes superfood vitamins are better, here's why.</span></li>
+						</ul>
+						<img src="<?php echo get_template_directory_uri(); ?>/images/clickme.png">
+					</div>
+					<img class="v-divider" src="<?php echo get_template_directory_uri(); ?>/images/vertical.png">
+					<div class="half">
+						<h4><span>Blog</span> goodness</h4>
+						<hr/>
+						<ul><?php query_posts('posts_per_page=3'); ?>
+							<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+								<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+							<?php endwhile; endif; ?>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+<?php get_footer(); ?>
---
+++ Website/new-hp.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+  <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/normalize.css">
+  <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/newhp2.css">
+  <link type="text/css" rel="stylesheet" href="http://fast.fonts.com/cssapi/ae0e63e9-c5cf-4919-a2b1-2fdf7c96485b.css"/>
+  <script type="text/javascript" src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/functions.js"></script>
+</head>
+<body>
+ <div class="header">
+   <div class="container">
+    <a href="#"><img class="logo" src="<?php echo get_template_directory_uri(); ?>/images/logo.png" /></a>
+    <div class="nav">
+      <?php wp_nav_menu( array('menu' => 'Home Menu' )); ?>
+    </div>
+  </div>
+</div>
